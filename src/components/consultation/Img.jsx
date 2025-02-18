4ee3a3b8ec31451306ddb11b39b57e13ea8ce623
Khurshid Alam <khurshid1556@gmail.com>
2025-02-18 23:03:56 +0530

    .

--- src/components/consultation/Img.jsx
+++ src/components/consultation/Img.jsx
@@ -1,11 +1,13 @@
-
-
 const Img = () => {
   return (
     <div className="w-1/3 ">
-        <img src="Slicing/header/about-image.jpg" alt="" className="w-full h-full object-cover object-center" />
+      <img
+        src="Slicing/header/about-image.jpg"
+        alt=""
+        className="w-full h-full object-cover object-center"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Img
+export default Img;
